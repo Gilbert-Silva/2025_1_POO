60ca6c882fae8ce4d22a9517eba89e093e378630
Gilbert-Silva <gilbertif1995@outlook.com>
2025-05-07 16:20:58 +0000

    ListaPoo04

--- Aula_09/listapoo04ex02.py
+++ Aula_09/listapoo04ex02.py
@@ -18,6 +18,44 @@ class Musica:
     def __str__(self):
         return f"{self.__titulo} - {self.__artista} - {self.__album}"    
 
+class UI:
+    @staticmethod
+    def menu():
+        return int(input("Menu: 1-Criar Playlist, 2-Inserir música, 3-Listar músicas, 9-Fim: "))
+
+    @staticmethod
+    def main():
+        op = 0
+        while op != 9:
+            op = UI.menu()
+            if op == 1: x = UI.criar_playlist()
+            if op == 2: UI.inserir_musica(x)
+            if op == 3: UI.listar_musicas(x)
+
+    @staticmethod
+    def criar_playlist(): # método da classe
+        nome = input("Informe o nome da playlist: ")
+        descricao = input("Informe uma descrição: ")
+        x = PlayList(nome, descricao)
+        return x
+
+    @staticmethod
+    def inserir_musica(x): # método da classe
+        titulo = input("Informe o título da música: ")
+        artista = input("Informe o artista/banda: ")
+        album = input("Informe o álbum: ")
+        musica = Musica(titulo, artista, album)
+        x.inserir(musica)
+
+    @staticmethod
+    def listar_musicas(x): # método da classe
+        print(x)
+        for musica in x.listar():
+            print("  ", musica)
+
+UI.main()    
+
+"""
 x = PlayList("Rock", "Minhas música de rock preferidas")
 y = PlayList("Axé", "Show de Ivete Sangalo - Maracanã")
 m1 = Musica("Hotel Califórnia", "Eagles", "Eagles")
@@ -41,5 +79,5 @@ for musica in y.listar():
 #print(m2)
 #print(m3)
 
-
+"""
         
