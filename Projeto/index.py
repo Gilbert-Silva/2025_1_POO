55d37257e377fd40f47fb77bfc74837756b2702a
Gilbert-Silva <gilbertif1995@outlook.com>
2025-06-16 20:21:43 +0000

    Streamlit

---
+++ Projeto/index.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from retanguloUI import RetanguloUI
+from retanguloUI2 import RetanguloUI2
+
+#RetanguloUI.main()
+RetanguloUI2.main()
+
+#st.header("POO em Python com Streamlit")
+#if st.button("Clique Aqui"):
+#    st.write("Bem-vindo(a) ao Streamlit")
+
